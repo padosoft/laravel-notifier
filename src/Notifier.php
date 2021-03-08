ff38710c85ae62466c0ead613698ed815dca7843
Alberto Mori <alberto@flatiron.it>
2021-03-08 14:57:37 +0100

    Fixato Notifier

--- src/Notifier.php
+++ src/Notifier.php
@@ -15,6 +15,13 @@ class Notifier
     protected $notifications = [];
 
     /**
+     * Flashable added notifications
+     *
+     * @var array
+     */
+    protected $flashedNotifications = [];
+
+    /**
      * Added notifications
      *
      * @var array
@@ -38,6 +45,13 @@ class Notifier
     public function __construct(SessionManager $session)
     {
         $this->session = $session;
+        $flashedNotifications = $this->session->get('laravel::flash-notifications');
+        if ($flashedNotifications !== null) {
+            $this->flashedNotifications = $flashedNotifications;
+        }
+        if($flashedNotifications === null){
+            $this->flashedNotifications = [];
+        }
     }
 
     /**
@@ -48,51 +62,20 @@ class Notifier
      */
     public function render(): string
     {
-        $notifications = $this->session->get('laravel::notifications');
-        if (!$notifications) {
-            $notifications = [];
+        $output = [];
+
+        foreach ($this->notifications as $notification) {
+
+            $output[] = $notification;
+        }
+        foreach ($this->flashedNotifications as $flNotification) {
+
+            $output[] = $flNotification;
         }
 
-        $output = '';
+        $this->session->forget('laravel::flash-notifications');
 
-        foreach ($notifications as $notification) {
-
-            $output .= $this->renderNotification($notification);
-        }
-        $this->session->forget('laravel::notifications');
-
-        return $output;
-    }
-
-    /**
-     * Render the script of given notification to insert into script tag
-     *
-     * @param $notification
-     *
-     * @return string
-     */
-    public function renderNotification($notification): string
-    {
-        $theme = (isset($notification['theme']) ? $notification['theme'] : 'metroui');
-        $timeout = ((isset($notification['timeout']) && $notification['timeout'] != '' && is_int($notification['timeout']) && $notification['timeout'] > 0) ? $notification['timeout'] : false);
-        $text = (isset($notification['text']) ? str_replace("'", "\\'", $notification['text']) : null);
-        $message = (isset($notification['message']) ? $notification['message'] : '');
-        $type = (isset($notification['type']) ? $notification['type'] : 'info');
-        $layout = (isset($notification['layout']) ? $notification['layout'] : 'topRight');
-
-        $output = "
-                $(function () {
-                    new Noty({
-						theme: '" . $theme . "',
-						timeout: '" . $timeout . "',
-						type: '" . $type . "',
-						layout: '" . $layout . "',
-						text: '" . $text . "'
-					}).show();
-                });
-				";
-
-        return $output;
+        return "window.notifications = " . json_encode($output) . ";";
     }
 
     /**
@@ -105,25 +88,37 @@ class Notifier
      * @param bool $onlyNextRequest if true(default), se the notification in session only for the next request
      *
      */
-    public function add($theme, $timeout, $type, $layout, $text, array $options = [], bool $onlyNextRequest = true)
+    public function add($theme, $timeout, $type, $layout, $text, $sounds = null, $soundsVolume = null, bool $onlyNextRequest = true)
     {
         if ($type == '') {
             $type = 'info';
         }
 
-        $this->notifications[] = [
+        if ($timeout === null) {
+            $timeout = false;
+        }
+
+        $notification = [
             'theme' => $theme,
             'timeout' => $timeout,
             'type' => $type,
             'layout' => $layout,
             'text' => $text,
-            'options' => $options
         ];
+        if($sounds !== null){
+            $notification['sources'] = [
+                'sounds' => [$sounds],
+                'soundsVolume' => $soundsVolume !== null ? $soundsVolume : 0.5,
+            ];
+        }
 
         if ($onlyNextRequest) {
-            $this->session->flash('laravel::notifications', $this->notifications);
+            $flashNotifications = $this->session->get('laravel::flash-notifications');
+            $this->session->forget('laravel::flash-notifications');
+            $flashNotifications[] = $notification;
+            $this->session->flash('laravel::flash-notifications', $flashNotifications);
         } else {
-            $this->session->put('laravel::notifications', $this->notifications);
+            $this->notifications[] = $notification;
         }
     }
 
@@ -134,13 +129,13 @@ class Notifier
      * @param string $title The notification's title
      * @param array $options
      */
-    public function info($text, array $options = [])
+    public function info($text, $onlyNextRequest = true, array $options = [])
     {
         $theme = (isset($options['theme']) && $options['theme'] != '') ? $options['theme'] : 'metroui';
-        $timeout = (isset($options['timeout']) && $options['timeout'] != '' && is_int($options['timeout'])) ? $options['timeout'] : 0;
-        $position = (isset($options['position']) && $options['position'] != '') ? $options['position'] : 'topRight';
+        $timeout = (isset($options['timeout']) && $options['timeout'] != '' && is_int($options['timeout'])) ? $options['timeout'] : false;
+        $layout = (isset($options['layout']) && $options['layout'] != '') ? $options['layout'] : 'topRight';
 
-        $this->add($theme, $timeout, 'info', $position, $text, array(), true);
+        $this->add($theme, $timeout, 'info', $layout, $text, null, null, $onlyNextRequest);
     }
 
     /**
@@ -150,13 +145,13 @@ class Notifier
      * @param string $title The notification's title
      * @param array $options
      */
-    public function error($text, array $options = [])
+    public function error($text, $onlyNextRequest = true, array $options = [])
     {
         $theme = (isset($options['theme']) && $options['theme'] != '') ? $options['theme'] : 'metroui';
         $timeout = (isset($options['timeout']) && $options['timeout'] != '' && is_int($options['timeout'])) ? $options['timeout'] : 0;
-        $position = (isset($options['position']) && $options['position'] != '') ? $options['position'] : 'topRight';
+        $layout = (isset($options['layout']) && $options['layout'] != '') ? $options['layout'] : 'topRight';
 
-        $this->add($theme, $timeout, 'error', $position, $text, array(), true);
+        $this->add($theme, $timeout, 'error', $layout, $text, null, null, $onlyNextRequest);
     }
 
     /**
@@ -166,13 +161,13 @@ class Notifier
      * @param string $title The notification's title
      * @param array $options
      */
-    public function warning($text, array $options = [])
+    public function warning($text, $onlyNextRequest = true, array $options = [])
     {
         $theme = (isset($options['theme']) && $options['theme'] != '') ? $options['theme'] : 'metroui';
         $timeout = (isset($options['timeout']) && $options['timeout'] != '' && is_int($options['timeout'])) ? $options['timeout'] : 0;
-        $position = (isset($options['position']) && $options['position'] != '') ? $options['position'] : 'topRight';
+        $layout = (isset($options['layout']) && $options['layout'] != '') ? $options['layout'] : 'topRight';
 
-        $this->add($theme, $timeout, 'warning', $position, $text, array(), true);
+        $this->add($theme, $timeout, 'warning', $layout, $text, null, null, $onlyNextRequest);
     }
 
     /**
@@ -182,13 +177,13 @@ class Notifier
      * @param string $title The notification's title
      * @param array $options
      */
-    public function success($text, array $options = [])
+    public function success($text, $onlyNextRequest = true, array $options = [])
     {
         $theme = (isset($options['theme']) && $options['theme'] != '') ? $options['theme'] : 'metroui';
         $timeout = (isset($options['timeout']) && $options['timeout'] != '' && is_int($options['timeout'])) ? $options['timeout'] : 0;
-        $position = (isset($options['position']) && $options['position'] != '') ? $options['position'] : 'topRight';
+        $layout = (isset($options['layout']) && $options['layout'] != '') ? $options['layout'] : 'topRight';
 
-        $this->add($theme, $timeout, 'success', $position, $text, array(), true);
+        $this->add($theme, $timeout, 'success', $layout, $text, null, null, $onlyNextRequest);
     }
 
 
@@ -197,12 +192,27 @@ class Notifier
      *
      * @param bool $withSession if true (default) clean notifications in session too.
      */
-    public function clear(bool $withSession = true)
+    public function clear()
     {
         $this->notifications = [];
+    }
+
+    public function clearFlashed(bool $withSession = true)
+    {
+        $this->flashedNotifications = [];
 
         if ($withSession) {
-            $this->session->forget('laravel::notifications');
+            $this->session->forget('laravel::flash-notifications');
+        }
+    }
+
+    public function clearAll(bool $withSession = true)
+    {
+        $this->notifications = [];
+        $this->flashedNotifications = [];
+
+        if ($withSession) {
+            $this->session->forget('laravel::flash-notifications');
         }
     }
 }
